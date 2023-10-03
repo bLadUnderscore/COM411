4ea2a1e98d963ecc73595cf7d18338632e43cb45
FREDDIE ROBINSON <0robif41@solent.ac.uk>
2023-10-03 11:35:40 +0100

    Added comments

--- week2/nested if.py
+++ week2/nested if.py
@@ -1,8 +1,8 @@
-cover = input("Enter the cover type of your book: \n").lower()
+cover = input("Enter the cover type of your book: \n").lower() # Asks the user what type of cover is their book
 
 if cover == "soft":
     bound = ""
-    while bound != "Y" and bound != "N":
+    while bound != "Y" and bound != "N": # If bound doesnt equal Yes or No, the Code will display an error and loop
         bound = input("Is the book perfect-bound? Y/N \n").upper()
         if bound == "Y":
             print("Soft cover, perfect bound books are very popular!")
