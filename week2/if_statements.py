0fdb9f9037369a0025ad0c4e406ef15a49e1e2dd
FREDDIE ROBINSON <0robif41@solent.ac.uk>
2023-10-03 11:08:27 +0100

    Book Adventure Genre

---
+++ week2/if_statements.py
@@ -0,0 +1,9 @@
+genre = input("What type of book is this \n")
+
+if genre = "adventure":
+    print(" I like adventure books")
+else:
+    print("Not really a big fan of that genre")
+    quit()
+
+print("Finised reading book.")
